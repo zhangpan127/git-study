bd8136ed5883947f7d728f9f250d55b8b43c9267
zhang pan <zhangpan01@corp.netease.com>
2017-07-24 09:46:57 +0800

    git学习，初始化仓库内容。

---
+++ hello/src/main/java/com/netease/zp/hello/AppMain.java
@@ -0,0 +1,13 @@
+package com.netease.zp.hello;
+
+/**
+ * @description: git学习
+ * @date: 2017/7/24
+ * @author: zhangpan01
+ * @email: zhangpan01@corp.netease.com
+ */
+public class AppMain {
+    public static void main(String[] args){
+        System.out.println("Hell git");
+    }
+}
