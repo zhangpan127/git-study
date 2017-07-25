90e9ec02004fa16b3872172b10d0169512b94010
zhang pan <zhangpan01@corp.netease.com>
2017-07-25 14:42:36 +0800

    branch05提交

---
+++ hello/src/main/java/com/netease/zp/hello/branch/Branch05.java
@@ -0,0 +1,13 @@
+package com.netease.zp.hello.branch;
+
+/**
+ * @description:
+ * @date: 2017/7/25
+ * @author: zhangpan01
+ * @email: zhangpan01@corp.netease.com
+ */
+public class Branch05 {
+    public static void main(String[] args){
+        System.out.println("Hello branch05");
+    }
+}
