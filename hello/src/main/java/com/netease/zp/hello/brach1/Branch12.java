eac8a968f77bf5c8b9ac727a12dc9be55454a1cd
zhang pan <zhangpan01@corp.netease.com>
2017-07-25 14:54:15 +0800

    branch12功能添加

---
+++ hello/src/main/java/com/netease/zp/hello/brach1/Branch12.java
@@ -0,0 +1,13 @@
+package com.netease.zp.hello.brach1;
+
+/**
+ * @description: 分支12的创建
+ * @date: 2017/7/25
+ * @author: zhangpan01
+ * @email: zhangpan01@corp.netease.com
+ */
+public class Branch12 {
+    public static void main(String[] args){
+        System.out.println("Hello branch12");
+    }
+}
